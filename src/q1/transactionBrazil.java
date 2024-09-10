0c0ee142a89caae3fae7f4739c78f3d8498039f7
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2024-09-10 20:35:46 -0300

    [Q1] Logic finish, falta só escrever no arquivo

--- src/q1/transactionBrazil.java
+++ src/q1/transactionBrazil.java
@@ -14,13 +14,11 @@ import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.BasicConfigurator;
 import java.io.IOException;
 
-//Número de transações envolvendo o Brasil.
+// Número de transações envolvendo o Brasil.
 public class transactionBrazil {
     public static void main(String[] args) throws Exception  {
         BasicConfigurator.configure();
         Configuration c = new Configuration();
-        String[] file = new GenericOptionsParser(c, args).getRemainingArgs();
-
         // Job creation
         Job job = Job.getInstance(c, "transactionBrazil");
 
@@ -29,6 +27,55 @@ public class transactionBrazil {
         // Output file
         Path outputFile = new Path("output/question1.txt");
 
+        // Set classes
+        job.setJarByClass(transactionBrazil.class);
+        job.setJarByClass(Map.class);
+        job.setJarByClass(Reduce.class);
 
+        // definicao dos tipos de saida
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // Input and Output files registration
+        FileInputFormat.addInputPath(job, inputFile);
+        FileOutputFormat.setOutputPath(job, outputFile);
+
+        // Job and wait for execution
+        boolean success = job.waitForCompletion(true);
+        if (!success) {
+            System.err.println("Job failed.");
+        }
+    }
+
+    public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+            String linha = value.toString();
+
+            if (linha.startsWith("country")) {
+                return;
+            }
+
+            String[] col = linha.split(";");
+
+            if (col[0].equals("Brazil")) {
+                Text chavesaida = new Text(col[0]);
+                con.write(chavesaida, new IntWritable(1));
+            }
+        }
+    }
+
+    public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException {
+            int sum = 0;
+
+            for (IntWritable value : values){
+                sum += value.get();
+            }
+
+            con.write(key,new IntWritable(sum));
+        }
     }
 }
+
