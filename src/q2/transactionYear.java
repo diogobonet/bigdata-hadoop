932e0205f0b243d8467a839189e8057894a2920b
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2024-09-10 22:34:41 -0300

    [Q2] Started

--- src/q2/transactionYear.java
+++ src/q2/transactionYear.java
@@ -1,4 +1,86 @@
 package q2;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.log4j.BasicConfigurator;
+import java.io.IOException;
+
 public class transactionYear {
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+
+        Configuration c = new Configuration();
+        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
+        // arquivo de entrada
+        Path input = new Path("in/operacoes_comerciais_inteira.csv");
+
+        // arquivo de saida
+        Path output = new Path("output/pergunta2.txt");
+
+        // criacao do job e seu nome
+        Job j = new Job(c, "jobano");
+
+        // registro das classes
+        j.setJarByClass(transactionYear.class);
+        j.setMapperClass(Map2.class);
+        j.setReducerClass(Reduce2.class);
+
+        // definicao dos tipos de saida
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(IntWritable.class);
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(IntWritable.class);
+
+        // cadastro dos arquivos de entrada e saida
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
+
+        // lanca o job e aguarda sua execucao
+        System.exit(j.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class Map2 extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        // Funcao de map
+        public void map(LongWritable key, Text value, Context con)
+                throws IOException, InterruptedException {
+            String linha = value.toString();
+
+            if (linha.startsWith("country")){
+                return;
+            }
+
+            String[] col = linha.split(";");
+
+            Text chave = new Text(col[1]);
+
+            con.write(chave,new IntWritable(1));
+        }
+    }
+
+    public static class Reduce2 extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        // Funcao de reduce
+        public void reduce(Text key, Iterable<IntWritable> values, Context con)
+                throws IOException, InterruptedException {
+
+            int soma = 0;
+
+            for (IntWritable valor:values){
+                soma += valor.get();
+            }
+
+            con.write(key,new IntWritable(soma));
+        }
+    }
 }
+
